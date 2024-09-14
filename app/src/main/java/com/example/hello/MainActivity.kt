27dd436acb6945cd4781ddc3d58537b9f5e7f10e
王瑞泽 <475825096@qq.com>
2024-09-14 22:28:40 +0800

    base constructor

---
+++ app/src/main/java/com/example/hello/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.hello
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        bottomNavigationView.setupWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/com/example/hello/ui/dashboard/ContactsFragment.kt
@@ -0,0 +1,17 @@
+package com.example.hello.ui.dashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.hello.R
+
+class ContactsFragment:Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_contacts, container, false)
+    }
+}
